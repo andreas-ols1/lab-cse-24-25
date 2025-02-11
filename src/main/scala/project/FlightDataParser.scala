d0522c96473192ce5cb530abf4367822be90ecc8
Andreas Omholt Olsen <andreaoo@stud.ntnu.no>
2025-02-11 19:00:34 +0100

    Flight data parser class

---
+++ src/main/scala/project/FlightDataParser.scala
@@ -0,0 +1,42 @@
+package project
+
+object FlightDataParser {
+  def getInt(str:String) : Int = {
+    if (str.forall(Character.isDigit))
+      str.toInt
+    else
+      -1
+  }
+
+  def parse_flight(line: String): (Int, Int, Int, String, Int, String, Int, String, String, String, String, String) = {
+    val parts = line.split(",")
+    val year = getInt(parts(0))
+    val month = getInt(parts(1))
+    val day = getInt(parts(2))
+    val dep_time = parts(4)
+    val dep_delay = getInt(parts(15))
+    val arr_time = parts(6)
+    val arr_delay = getInt(parts(14))
+    val carrier = parts(8)
+    val flightnum = parts(9)
+    val tailnum = parts(10)
+    val origin = parts(16)
+    val dest = parts(17)
+    (year, month, day, dep_time, dep_delay, arr_time, arr_delay, carrier, tailnum, flightnum, origin, dest)
+  }
+
+  def parse_aircraft(line: String): (String, String, String, String, String, Int) = {
+    val parts = line.split(",")
+    if (parts.length != 9) {
+      (parts(0), "", "", "", "", 0)
+    } else {
+      val tailnum: String = parts(0)
+      val manufacturer = parts(2)
+      val model = parts(4)
+      val type_aircraft = parts(6)
+      val engine_type = parts(7)
+      val year = getInt(parts(8))
+      (tailnum, manufacturer, model, type_aircraft, engine_type, year)
+    }
+  }
+}
